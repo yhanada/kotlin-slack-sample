bf475b0d60f8f2c933ac65120852e831526c01b9
Yoshihito Hanada <hanada@gmail.com>
2017-11-20 02:06:16 +0900

    added initial files

---
+++ src/main/kotlin/com/github/yhanada/slackbot/Main.kt
@@ -0,0 +1,9 @@
+package com.github.yhanada.slackbot
+
+class Main {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            println("hello world")
+        }
+    }
+}
